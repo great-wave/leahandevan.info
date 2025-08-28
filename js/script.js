50d98e56de3f5ffeeeeaa37122c8ab815e63f928
Evan Bluestein <evan@telegraphicstyle.com>
2025-08-28 16:38:06 -0700

    added elements and functionality to homepage

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ function checkPassword() {
     if (password === 'celebrate') {
         document.getElementById('passwordPrompt').classList.add('hidden');
         document.getElementById('mainContent').classList.remove('hidden');
+        document.getElementById('mainContent').classList.add('flex-col');
+
     } else {
         alert('Wrong password');
     }
