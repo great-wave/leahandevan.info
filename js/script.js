cb612d4b138408ecdd6e62be0b950c55f53fb669
Evan Bluestein <evan@telegraphicstyle.com>
2025-08-28 17:25:27 -0700

    adjusted hide and show

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ function checkPassword() {
     if (password === 'celebrate') {
         document.getElementById('passwordPrompt').classList.add('hidden');
         document.getElementById('mainContent').classList.remove('hidden');
+        document.getElementById('mainContent').classList.add('flex');
         document.getElementById('mainContent').classList.add('flex-col');
 
     } else {
